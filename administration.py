505e791cf189366cdebd57bf2e5df539e2986534
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-07-24 14:06:23 -0500

    Local MAIN: add option 4 and option 5

--- administration.py
+++ administration.py
@@ -7,15 +7,17 @@ def run():
     1)Registrar Ingresos
     2)Registrar Egresos
     3)Ver presupuesto actual
+    4)Ver lista de Ingresos y Egresos
+    5)Salir
     Escribe el numero de la opcion aqui ==>  """
     opcion = int(input(menu))
-    while opcion <=3 :
+    while opcion <=5 :
         #OPCION 1 REGISTRAR INGRESO:
         if opcion == 1:
             os.system("cls")
             with open ("./ingresos.py","a",encoding="utf-8") as ai:
                 print("Escribe (0) cuando termines")
-                ingreso = input("Escribe el ingreso: ")
+                ingreso = input("Escribe el ingreso:$. ")
                 ai.write("\n")
                 ai.write(ingreso)
                 if ingreso == "0":
@@ -27,7 +29,7 @@ def run():
             os.system("cls")
             with open ("./egresos.py","a",encoding="utf-8") as ae:
                 print("Escribe (0) cuando termines")
-                egreso = input("Escribe el egreso: ")
+                egreso = input("Escribe el egreso:$. ")
                 ae.write("\n")
                 ae.write(egreso)
                 if egreso == "0":
@@ -39,13 +41,32 @@ def run():
             os.system("cls")
             with open ("./ingresos.py","r",encoding="utf-8")as ai:
                 total_ingresos =sum([int(line) for line in ai]) 
-            print(f"Total ingresos = $ {total_ingresos}")
+            print(f"Total ingresos = $. {total_ingresos}")
             with open ("./egresos.py","r",encoding="utf-8")as ae:
                 total_egresos = sum([int(line) for line in ae])
-            print(f"Total de egresos = $ {total_egresos}")
-            print(f"El presupuesto total es = $ {total_ingresos-total_egresos} ")
+            print(f"Total de egresos = $. {total_egresos}")
+            print(f"El presupuesto total es = $. {total_ingresos-total_egresos} ")
             time.sleep(3.5)
-            break
+            return run()
+        #OPCION 4 VER LISTA INGRESOS Y EGRESOS:
+        if opcion == 4:
+            os.system("cls")
+            with open ("./ingresos.py","r",encoding="utf-8")as ai:
+                lista_ai = ""
+                for line in ai:
+                    lista_ai = ("$."+line) + lista_ai
+            with open ("./egresos.py","r",encoding="utf-8")as ae:
+                lista_ae = ""
+                for line in ae:
+                    lista_ae = ("$."+line) + lista_ae
+            print("Ingresos:" +("\n")+(lista_ai)) 
+            print("Egresos:" +("\n")+(lista_ae))
+            time.sleep(3)
+            return run()
+        #OPCION 5 SALIR    
+        if opcion == 5:
+            exit()
+
     else:
         print("Ingresa una opcion correcta:")
         return run()
